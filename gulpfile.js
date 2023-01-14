9ee12267034d60046951421ce74df0d8d79a3fa5
Galina <vikhrovagp@gmail.com>
2023-01-15 00:10:54 +0300

    version1: fix mark-up, fix styles, gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const {src, dest, series, watch} = require('gulp')
-const less = require('gulp-less')
-const csso = require('gulp-csso')
+
 const include = require('gulp-file-include');
 const minify = require('gulp-htmlmin')
-const clean = require('gulp-clean')
-const concat = require('gulp-concat')
+const less = require('gulp-less')
+const csso = require('gulp-csso')
 const autoprefixer = require('gulp-autoprefixer')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const newer = require('gulp-newer')
+const fonter = require('gulp-fonter')
+const ttf2woff2 = require('gulp-ttf2woff2')
+const concat = require('gulp-concat')
+const clean = require('gulp-clean')
 const sync = require('browser-sync').create()
 
 function html() {
@@ -30,25 +36,29 @@ function compileLess() {
     .pipe(dest('dist'))
 }
 
-function copyImg() {
-  return src([
-    "src/img/**/*.{png,jpg,svg}"
-  ])
-    .pipe(dest("dist/media/img"));
+function js() {
+  return src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(dest('dist/js'))
+}
+
+function img() {
+  return src('src/img/**/*.{png,jpg,svg}')
+    .pipe(newer('dist/media/img'))
+    .pipe(imagemin({
+      verbose: true
+    }))
+    .pipe(dest('dist/media/img'))
 }
 
 function copyFonts() {
-  return src([
-    "src/fonts/**"
-  ])
-    .pipe(dest("dist/media/fonts"));
+  return src('src/fonts/**')
+    .pipe(dest('dist/media/fonts'));
 }
 
 function copyLibs() {
-  return src([
-    "src/libs/**"
-  ])
-    .pipe(dest("dist/libs"));
+  return src('src/libs/**')
+    .pipe(dest('dist/media/libs'));
 }
 
 function clear() {
@@ -66,11 +76,15 @@ function serve() {
     server: './dist'
   })
 
-  watch('src/**/*.html', series(html)).on('change', sync.reload)
-  watch('src/less/**/*.less', series(compileLess)).on('change', sync.reload)
+  watch('src/**/*.html', series(html)).on('change', sync.reload);
+  watch('src/less/**/*.less', series(compileLess)).on('change', sync.reload);
+  watch('src/js/**/*.js', series(js)).on('change', sync.reload);
+  watch('src/img/**/*.*', series(img)).on('all', sync.reload);
+  watch('src/fonts/**/*.*', series(copyFonts)).on('all', sync.reload);
+  watch(['src/css/libs'], series(copyLibs)).on('all', sync.reload);
 }
 
-exports.serve = series(clear, copyImg, copyFonts, copyLibs, compileLess, html, serve)
+exports.serve = series(clear, copyFonts, copyLibs, img, js, compileLess, html, serve);
 
 exports.clear = clear
 exports.clearMedia = clearMedia
